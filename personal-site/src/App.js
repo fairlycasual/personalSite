34e1a70b9746fea40435ca400a2ce95795764192
Will Howard <willh91@msn.com>
2018-08-03 18:40:44 -0600

    landing page not working, trouble rendering properly. Switching to implement Modals

--- personal-site/src/App.js
+++ personal-site/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 import './App.css';
 
 import LandingPage from './Landing.jsx';
--- personal-site/src/Container.jsx
+++ personal-site/src/Container.jsx
@@ -41,42 +41,44 @@ class Home extends React.Component {
 
   render () {
     return (
-      <div className="box">
-        <div id="title">
-          Will Howard
-        </div>
-        <div id="description">
-          <table >
-            <tbody>
-              <tr>
-                <td colSpan='2' >
-                  <a href="https://www.github.com/fairlycasual">
-                    <div>
-                      GitHub <FontAwesomeIcon icon={['fab', 'github']} />
-                    </div>
-                  </a>
-                </td>
-                <td colSpan='2'>
-                  <a href="https://linkedin.com/in/wph91">
-                    <div>
-                        LinkedIn <FontAwesomeIcon icon={['fab', 'linkedin']} />
-                    </div>
-                  </a>
-                </td>
-                <td colSpan='2'>
-                  <a href="https://docs.google.com/document/d/1GnRfgdXfp1opdutuVWvLRYdvcFepkHWDDV5IkI51UYo/edit?usp=sharing">
-                    <div>
-                      Resume <FontAwesomeIcon icon={['fab', 'file']} />
-                    </div>
-                  </a>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-          <Description currentPage={this.state.page} nextPage={this.nextPage} previousPage={this.previousPage}/>
-        </div>
-        <div className="photo">
-          <Photo currentPhoto={this.state.page}/>
+      <div className="container"> {/* not sure I need this div> */}
+        <div className="box">
+          <div id="title">
+            Will Howard
+          </div>
+          <div id="description">
+            <table >
+              <tbody>
+                <tr>
+                  <td colSpan='2' >
+                    <a href="https://www.github.com/fairlycasual">
+                      <div>
+                        GitHub <FontAwesomeIcon icon={['fab', 'github']} />
+                      </div>
+                    </a>
+                  </td>
+                  <td colSpan='2'>
+                    <a href="https://linkedin.com/in/wph91">
+                      <div>
+                          LinkedIn <FontAwesomeIcon icon={['fab', 'linkedin']} />
+                      </div>
+                    </a>
+                  </td>
+                  <td colSpan='2'>
+                    <a href="https://docs.google.com/document/d/1GnRfgdXfp1opdutuVWvLRYdvcFepkHWDDV5IkI51UYo/edit?usp=sharing">
+                      <div>
+                        Resume <FontAwesomeIcon icon={['fab', 'file']} />
+                      </div>
+                    </a>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <Description currentPage={this.state.page} nextPage={this.nextPage} previousPage={this.previousPage}/>
+          </div>
+          <div className="photo">
+            <Photo currentPhoto={this.state.page}/>
+          </div>
         </div>
       </div>
     );

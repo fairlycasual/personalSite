321e2bac4e2bd91c060ce108c1f44a49bc6878d7
Will Howard <willh91@msn.com>
2018-08-09 22:38:52 -0600

    container UI layout complete

--- personal-site/src/Academics.jsx
+++ personal-site/src/Academics.jsx
@@ -1,6 +1,20 @@
 import React from 'react';
 
 class Academics extends React.Component {
+
+  // react is powerful due to one-way data flow, this is why you implement redux (or context API!!!), for a single source of truth
+  // I'm going to lazily implement these Modals (?) by making Academics stateful vs prop drilling through?
+  constructor (props) {
+    super(props);
+    this.state = {
+      activeModal: null/*set to a number that's the key of the modals object*/,
+
+
+    }
+  }
+
+  // function that onClick sets the active modal to 
+
   render () {
     return (
     <p>
@@ -9,7 +23,7 @@ class Academics extends React.Component {
         As a Research Associate at MesoWest, I endeavored on large-scale validation of instrumentation for NASA, the GSLO<sub>3</sub>S research campaign, and deployed and maintained a network of surface weather stations.
         {/* TODO! Modal of research posters for all projects!!! */}
         <br /> <br />
-        I also undertook a research project with Professor Dave Whiteman where we quantified the pollutants transported by fog in Persistant Cold Air Pool events. <br /> 
+        I also undertook a research project with Professor Emeritus Dave Whiteman where we quantified the pollutants transported by fog in Persistant Cold Air Pool events. <br /> 
     </p>
     )
   }
--- personal-site/src/App.js
+++ personal-site/src/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faFile } from '@fortawesome/free-solid-svg-icons'
 import './App.css';
 
-import LandingPage from './Landing.jsx';
-library.add(fab)
+//import LandingPage from './Landing.jsx';
+import Home from './Container.jsx';
+library.add(fab, faFile)
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <LandingPage />
+        <Home />
       </div>
     );
   }
